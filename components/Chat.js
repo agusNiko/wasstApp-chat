c37810f8ce51cad76cf6e3ddc46f29ded04ea8aa
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-06-28 10:51:25 +0200

    change name of the componets
    Screen1 --> Start.js
     and
    Screen2 ---> Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default class chat extends React.Component {
+export default class Chat extends React.Component {
   render() {
-    let { name } = this.props.route.params;
+    // let name  = this.props.route.params.name;
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Hello Screen2!</Text>
--- components/Start.js
+++ components/Start.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
 
-export default class Screen1 extends React.Component {
+export default class Start extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Hello Screen1!</Text>
         <Button
           title="Go to Screen 2"
-          onPress={() => this.props.navigation.navigate("Screen2")}
+          onPress={() => this.props.navigation.navigate("Chat")}
         />
       </View>
     );
