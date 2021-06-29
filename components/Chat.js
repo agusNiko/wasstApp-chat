81983742128724bde500f988f8db93dd3f9c9909
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-06-29 23:23:35 +0200

    add background color

--- components/Chat.js
+++ components/Chat.js
@@ -1,12 +1,26 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
 export default class Chat extends React.Component {
   render() {
-    // let name  = this.props.route.params.name;
+    let name = this.props.route.params.name;
+    let chatColor = this.props.route.params.color;
+    this.props.navigation.setOptions({ title: "Hi " + name });
+
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello Screen2!</Text>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: chatColor,
+        }}
+      >
+        <Text>Hi {name}</Text>
+        <Button
+          title="Go back to Start"
+          onPress={() => this.props.navigation.navigate("Start")}
+        />
       </View>
     );
   }
