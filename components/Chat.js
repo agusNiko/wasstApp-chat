ca67b9049445885c21033a2227c46d866d2495d4
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-06-30 13:01:58 +0200

    GiftedChat
    importing GiftedChat, adding the constructor because we need to work with the state, componentDidMount will add the regards message to the state.
    
    listViewProps allows to change background color.

--- components/Chat.js
+++ components/Chat.js
@@ -1,27 +1,56 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, ImageBackground } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+  componentDidMount() {
+    let name = this.props.route.params.name;
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer " + name,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
   render() {
     let name = this.props.route.params.name;
     let chatColor = this.props.route.params.color;
     this.props.navigation.setOptions({ title: "Hi " + name });
 
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: chatColor,
+      <GiftedChat
+        listViewProps={{
+          style: {
+            backgroundColor: chatColor,
+          },
+        }} // listVieProps allows me to change the color background of the GiftedChat
+        messages={this.state.messages}
+        onSend={(messages) => this.onSend(messages)}
+        user={{
+          _id: 1,
         }}
-      >
-        <Text>Hi {name}</Text>
-        <Button
-          title="Go back to Start"
-          onPress={() => this.props.navigation.navigate("Start")}
-        />
-      </View>
+      />
     );
   }
 }
