5b45e1c98e741c6ec6753d31952b2509cc11a522
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-07-01 20:38:26 +0200

    Platform bug fixed
    import Platform from react

--- components/Start.js
+++ components/Start.js
@@ -8,7 +8,9 @@ import {
   TouchableOpacity,
   ImageBackground,
   KeyboardAvoidingView,
+  Platform,
 } from "react-native";
+import { Bubble } from "react-native-gifted-chat";
 import Image from "./Background-Image.png"; //this is the way to import imgs to de used in the app.
 
 export default class Start extends React.Component {
@@ -21,25 +23,33 @@ export default class Start extends React.Component {
     const styles = StyleSheet.create({
       container: {
         flex: 1,
-        justifyContent: "center",
+        //justifyContent: "center",
         padding: "6%",
+      },
+      inner: {
+        padding: 24,
+        flex: 1,
+        justifyContent: "flex-end",
       },
       startViewUp: {
         flex: 56,
         //backgroundColor: "red",
-        // justifyContent: "center",
+        justifyContent: "center",
         alignItems: "center",
       },
       appTitle: {
         fontSize: 45,
+        height: 50,
         fontWeight: "600",
         color: "#FFFFFF",
+        margin: "auto",
       },
       startViewDown: {
-        flex: 44,
         backgroundColor: "white",
         justifyContent: "space-around",
         alignItems: "center",
+        position: "relative",
+        flex: 44,
       },
       textInput: {
         borderColor: "gray",
@@ -75,7 +85,7 @@ export default class Start extends React.Component {
 
     return (
       <KeyboardAvoidingView
-        behavior={Platform.OS === "android" ? "height" : null}
+        behavior={Platform.OS === "android" ? "height" : "padding"}
         style={{ flex: 1 }}
       >
         <ImageBackground
@@ -87,110 +97,112 @@ export default class Start extends React.Component {
           }}
         >
           <View style={[styles.container]}>
-            <View style={styles.startViewUp}>
-              <Text style={styles.appTitle}>WassApp!</Text>
-            </View>
-            <View style={styles.startViewDown}>
-              <TextInput
-                style={styles.textInput}
-                onChangeText={(name) => this.setState({ name })} // on change state.name is modified.
-                value={this.state.name}
-                placeholder="Type here ..."
-              />
+            <View style={[styles.inner]}>
+              <View style={styles.startViewUp}>
+                <Text style={styles.appTitle}>WassApp!</Text>
+              </View>
+              <View style={styles.startViewDown}>
+                <TextInput
+                  style={styles.textInput}
+                  onChangeText={(name) => this.setState({ name })} // on change state.name is modified.
+                  value={this.state.name}
+                  placeholder="Type here ..."
+                />
 
-              <View style={styles.backgroundColors}>
-                <View
-                  style={{
-                    borderColor: "red",
-                    height: 55,
-                    width: 55,
-                    borderWidth: 3,
-                    borderRadius: 100,
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
+                <View style={styles.backgroundColors}>
+                  <View
+                    style={{
+                      borderColor: "red",
+                      height: 55,
+                      width: 55,
+                      borderWidth: 3,
+                      borderRadius: 100,
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <TouchableOpacity
+                      className="colorButton1"
+                      style={{
+                        height: 50,
+                        width: 50,
+                        borderRadius: 100,
+                        borderWidth: 1,
+                        backgroundColor: "#090C08",
+                      }}
+                      onPress={() => {
+                        this.setState({ color: "#090C08" });
+                      }}
+                    >
+                      <View style={{ height: 50, width: 50 }} />
+                    </TouchableOpacity>
+                  </View>
                   <TouchableOpacity
-                    className="colorButton1"
+                    className="colorButton2"
                     style={{
                       height: 50,
                       width: 50,
                       borderRadius: 100,
                       borderWidth: 1,
-                      backgroundColor: "#090C08",
+                      backgroundColor: "#474056",
                     }}
                     onPress={() => {
-                      this.setState({ color: "#090C08" });
+                      this.setState({ color: "#474056" });
+                    }}
+                  >
+                    <View style={{ height: 50, width: 50 }} />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    className="colorButton3"
+                    style={{
+                      height: 50,
+                      width: 50,
+                      borderRadius: 100,
+
+                      backgroundColor: "#8A95A5",
+                    }}
+                    onPress={() => {
+                      this.setState({ color: "#8A95A5" });
+                    }}
+                  >
+                    <View style={{ height: 50, width: 50 }} />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    className="colorButton4"
+                    style={{
+                      height: 50,
+                      width: 50,
+                      borderRadius: 100,
+
+                      backgroundColor: "#B9C6AE",
+                    }}
+                    onPress={() => {
+                      this.setState({ color: "#B9C6AE" });
                     }}
                   >
                     <View style={{ height: 50, width: 50 }} />
                   </TouchableOpacity>
                 </View>
-                <TouchableOpacity
-                  className="colorButton2"
-                  style={{
-                    height: 50,
-                    width: 50,
-                    borderRadius: 100,
-                    borderWidth: 1,
-                    backgroundColor: "#474056",
-                  }}
-                  onPress={() => {
-                    this.setState({ color: "#474056" });
-                  }}
-                >
-                  <View style={{ height: 50, width: 50 }} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  className="colorButton3"
-                  style={{
-                    height: 50,
-                    width: 50,
-                    borderRadius: 100,
 
-                    backgroundColor: "#8A95A5",
-                  }}
-                  onPress={() => {
-                    this.setState({ color: "#8A95A5" });
-                  }}
-                >
-                  <View style={{ height: 50, width: 50 }} />
-                </TouchableOpacity>
                 <TouchableOpacity
-                  className="colorButton4"
                   style={{
-                    height: 50,
-                    width: 50,
-                    borderRadius: 100,
-
-                    backgroundColor: "#B9C6AE",
-                  }}
-                  onPress={() => {
-                    this.setState({ color: "#B9C6AE" });
+                    width: "88%",
                   }}
                 >
-                  <View style={{ height: 50, width: 50 }} />
+                  {/* here touchableOpacity allows me to increase the size of the button */}
+                  <Button
+                    style={styles.startChatButton}
+                    title="Start Chatting"
+                    onPress={
+                      () =>
+                        this.props.navigation.navigate("Chat", {
+                          name: this.state.name,
+                          color: this.state.color,
+                        }) //this code navigate to Chat screen and send name as a props
+                    }
+                  />
                 </TouchableOpacity>
               </View>
-
-              <TouchableOpacity
-                style={{
-                  width: "88%",
-                }}
-              >
-                {/* here touchableOpacity allows me to increase the size of the button */}
-                <Button
-                  style={styles.startChatButton}
-                  title="Start Chatting"
-                  onPress={
-                    () =>
-                      this.props.navigation.navigate("Chat", {
-                        name: this.state.name,
-                        color: this.state.color,
-                      }) //this code navigate to Chat screen and send name as a props
-                  }
-                />
-              </TouchableOpacity>
             </View>
           </View>
         </ImageBackground>
