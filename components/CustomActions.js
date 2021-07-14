655959f4c269ea1e92d0a1b6e30c73d5379149e0
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-07-14 06:56:31 +0200

    send pictures and location
    createn aync functions imagePicker,
    takePhoto and getLocation.
    
    incorporate this functions to onActionPress, to be call when user touches :
    "Choose From Library",
    "Take Picture", or
    "Send Location",

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,12 +1,82 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+//import permissions and imagepicker
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+import * as Location from "expo-location";
+import firebase from "firebase";
 
 export default class CustomActions extends React.Component {
   constructor(props) {
     super(props);
     this.state = { name: "", color: "" };
   }
+
+  // Image picker
+  imagePicker = async () => {
+    // expo permission
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    try {
+      if (status === "granted") {
+        // pick image
+        const result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images, // only images are allowed
+        }).catch((error) => console.log(error));
+        // canceled process
+        if (!result.cancelled) {
+          const imageUrl = await this.uploadImageFetch(result.uri);
+          this.props.onSend({ image: imageUrl });
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  takePhoto = async () => {
+    const { status } = await Permissions.askAsync(
+      Permissions.CAMERA,
+      Permissions.CAMERA_ROLL
+    );
+    try {
+      if (status === "granted") {
+        const result = await ImagePicker.launchCameraAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        }).catch((error) => console.log(error));
+
+        if (!result.cancelled) {
+          const imageUrl = await this.uploadImageFetch(result.uri);
+          this.props.onSend({ image: imageUrl });
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  getLocation = async () => {
+    try {
+      const { status } = await Permissions.askAsync(Permissions.LOCATION);
+      if (status === "granted") {
+        const result = await Location.getCurrentPositionAsync({}).catch(
+          (error) => console.log(error)
+        );
+        const longitude = JSON.stringify(result.coords.longitude);
+        const altitude = JSON.stringify(result.coords.latitude);
+        if (result) {
+          this.props.onSend({
+            location: {
+              longitude: result.coords.longitude,
+              latitude: result.coords.latitude,
+            },
+          });
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   onActionPress = () => {
     const options = [
@@ -25,12 +95,13 @@ export default class CustomActions extends React.Component {
         switch (buttonIndex) {
           case 0:
             console.log("user wants to pick an image");
-            return;
+            return this.imagePicker();
           case 1:
             console.log("user wants to take a photo");
-            return;
+            return this.takePhoto();
           case 2:
             console.log("user wants to get their location");
+            return this.getLocation();
           default:
         }
       }
